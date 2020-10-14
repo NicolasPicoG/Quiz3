f15f06460f9660f0ed848f0f2b0395392ed74edd
NicolasPicoG <nicolaspicog@gmail.com>
2020-10-13 20:41:55 -0500

    Actualización

--- src/planeta.js
+++ src/planeta.js
@@ -11,7 +11,8 @@ function Planeta(radius,textura,distancia){
 	this.mesh.position.x = distancia;
 	this.mesh.position.y = 0;
 	this.mesh.position.z = 0;
-	
+	//this.mesh.castShadow=true;
+	//this.mesh.receiveShadow = true;
 	this.mesh.name = "Planeta";
 
 	//objeto intermedio para realizar transformaciones
@@ -21,7 +22,6 @@ function Planeta(radius,textura,distancia){
 	this.transformacion.add(this.mesh);
 
 	this.animar=function(step,stepluna){
-		//Se añade la rotación de la tierra
 		this.mesh.rotation.y=step;
 		this.transformacion.rotation.y=step;
 		for(i=0;i<this.satelites.length;i++){
--- src/satelite.js
+++ src/satelite.js
@@ -2,12 +2,13 @@ function Satelite(radius,textura,distancia){
 	this.resolution=25;
 	this.geometry=new THREE.SphereGeometry(radius,this.resolution,this.resolution);
 	this.material=new THREE.MeshLambertMaterial({
-	map: THREE.ImageUtils.loadTexture(textura) });
+		map: THREE.ImageUtils.loadTexture(textura) });
 	this.mesh= new THREE.Mesh(this.geometry,this.material);
 	this.mesh.position.x = distancia;
 	this.mesh.position.y = 0;
 	this.mesh.position.z = 0;
-	
+	//this.mesh.castShadow=true;
+	//this.mesh.receiveShadow = true;
 	this.mesh.name = "Satelite";
 
 	//objeto intermedio para realizar transformaciones
@@ -15,7 +16,7 @@ function Satelite(radius,textura,distancia){
 
 	//añadimos el planeta a la transformacion
 	this.transformacion.add(this.mesh);
-//Se añade la animación para la rotación en el eje Y
+
 	this.animar=function(stepluna){
 		 this.transformacion.rotation.y=stepluna;
 	};
